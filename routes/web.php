2c068aeead50d11c47457f337d4144ede5cef7c1
cleyxcode <afiafikadewi3@gmail.com>
2026-04-24 10:34:14 +0900

    feat: dark mode portal orang tua dengan theme toggle

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
+// Storage route
+Route::get('/storage/{folder}/{filename}', function ($folder, $filename) {
+    $path = storage_path('app/public/' . $folder . '/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    $file = file_get_contents($path);
+    $type = mime_content_type($path);
+
+    return response($file, 200)->header('Content-Type', $type);
+})->where('folder', '.*')->where('filename', '.*');
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/profil', [HomeController::class, 'profil'])->name('profil');
 Route::get('/berita', [HomeController::class, 'berita'])->name('berita.index');
