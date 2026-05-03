2aa446408fd932da92df86bcef5f46545a4ba43a
cleyxcode <afiafikadewi3@gmail.com>
2026-05-03 21:30:50 +0900

    update ui admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,9 +27,13 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->login()
+            ->brandName('E-Raport SD 1 Passo')
+            ->font('Poppins')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#1e3a8a', // Warna biru tua (Dark Blue)
+                'info'    => \Filament\Support\Colors\Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
